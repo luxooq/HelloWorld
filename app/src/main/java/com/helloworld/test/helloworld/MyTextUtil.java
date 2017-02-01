d892cf81377efdf45e3ea9bf1a262b76228b501d
luxooq <xoxy@163.com>
2017-02-02 01:46:28 +0800

    1.第二次提交

--- app/src/main/java/com/helloworld/test/helloworld/MyTextUtil.java
+++ app/src/main/java/com/helloworld/test/helloworld/MyTextUtil.java
@@ -4,4 +4,8 @@ package com.helloworld.test.helloworld;
  * Created by Administrator on 17-2-2.
  */
 public class MyTextUtil {
+    public String getValue(){
+        return "Hello from the utility class";
+    }
+
 }
